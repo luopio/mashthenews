f92f6f06950c5be79f067d78ffaada617cea0069
unknown <Juho@.(none)>
2011-03-28 14:59:35 +0300

    rakenne

---
+++ mashthenews/src/test/Testi.java
@@ -0,0 +1,25 @@
+package test;
+import processing.core.*;
+
+
+public class Testi extends PApplet {
+
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		println("hello");
+		delay(2000);
+	}
+	
+	public static void main(String args[]) {
+		    PApplet.main(new String[] { "--present", "Testi" });
+	}
+	
+	
+	
+	
+	
+}
