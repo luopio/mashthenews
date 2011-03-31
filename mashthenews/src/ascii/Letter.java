f655fb21f28c4821bbac3006259ce55693e77a77
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2011-03-31 14:49:01 +0300

    updated gitignore, working paradise program

--- mashthenews/src/ascii/Letter.java
+++ mashthenews/src/ascii/Letter.java
@@ -1,29 +1,48 @@
 package ascii;
 
 import java.awt.geom.*;
+import org.jbox2d.collision.PolygonDef;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
 
 public class Letter {
 	
-	Point2D.Double speed;
-	Point2D.Double acceleration;
-	Point2D.Double position;
-	
+	private PolygonDef polygonDef;
+	private BodyDef bodyDef;
+	private Body body;
+
 	Paradise parent;
 	char letter;
 	
-	public Letter(Paradise parent, char letter) {
+	public Letter(Paradise parent, char letter, float x, float y) {
 		this.parent = parent;
 		this.letter = letter;
-		this.speed = new Point2D.Double();
-		this.acceleration = new Point2D.Double();
-		this.position = new Point2D.Double();
+	
+		// Dynamic body
+		polygonDef = new PolygonDef();
+		polygonDef.setAsBox(1.0f, 1.0f);
+		polygonDef.density = 1.0f;
+		polygonDef.friction = 0.3f;
+  
+		bodyDef = new BodyDef();
+		bodyDef.position.set(x, y);
+		
+		body = parent.getWorld().createBody(bodyDef);
+		body.createShape(polygonDef);
+		body.setMassFromShapes();
+	
 	}
 	
 	void draw() {
-		double pixelX = this.parent.width / Paradise.COLUMNS
-				* this.position.getX();
-		double pixelY = this.parent.height / Paradise.ROWS
-				* this.position.getY();
+		//double pixelX = this.parent.width / Paradise.COLUMNS
+		//		* this.position.getX();
+		//double pixelY = this.parent.height / Paradise.ROWS
+		//		* this.position.getY();
+		Vec2 pos = body.getPosition();
+		float pixelX = (int)pos.x * this.parent.scale.x;
+		float pixelY = (int)pos.y * this.parent.scale.y;
 		parent.text(letter, (int)pixelX, (int)pixelY);
 	}
 }
--- mashthenews/src/ascii/Paradise.java
+++ mashthenews/src/ascii/Paradise.java
@@ -1,32 +1,74 @@
 package ascii;
 
-import processing.core.PApplet;
 import processing.core.*;
+
+import org.jbox2d.collision.AABB;
+import org.jbox2d.collision.PolygonDef;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
 
 public class Paradise extends PApplet {
 
-	public static int ROWS = 200;
-	public static int COLUMNS = 200;
+	public static int COLUMNS = 80;
+	public static int ROWS = 60;
 	Letter letters[];
+	Vec2 scale;
+	
+	AABB aabb;
+	World world;
 	
 	public void setup() {
-		letters = new Letter[50];
+		// box2D stuff
+		aabb = new AABB();
+		//aabb.lowerBound = new Vec2(-(COLUMNS/2), -(ROWS/2));
+		//aabb.upperBound = new Vec2((COLUMNS/2), (ROWS/2));
+		aabb.lowerBound = new Vec2(0.0f, 0.0f);
+		aabb.upperBound = new Vec2(COLUMNS, ROWS);
+		
+		Vec2 gravity = new Vec2(0.0f, 0.1f);
+		world = new World(aabb, gravity, true);
+		world.setContinuousPhysics(true);
+		
+		// Create the ground.
+		// PolygonDef groundPolyDef = new PolygonDef();
+		// groundPolyDef.setAsBox(50.0f, 10.0f);
+		// groundPolyDef.density = 0.0f;
+		
+		// BodyDef groundBodyDef = new BodyDef();
+		// groundBodyDef.position.set(0.0f, -10.0f);
+		
+		// Body groundBody = world.createBody(groundBodyDef);
+		// groundBody.createShape(groundPolyDef);
+		
+		int w = 800; int h = 600;
+		scale = new Vec2();
+		scale.x = w/COLUMNS;
+		scale.y = h/ROWS;
+		
+		letters = new Letter[25];
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
 		for(int i = 0; i < letters.length; i++) {
 			char rndChar = alphabet.charAt( (int)random(alphabet.length()) );
-			letters[i] = new Letter(this, rndChar);
-			println("comon");
-			int r = (int)random(ROWS);
-			int c = (int)random(COLUMNS);
-			letters[i].position.setLocation(c, r);
-			letters[i].speed.setLocation(0, 0);
+			float r = random(ROWS);
+			float c = random(COLUMNS);
+			letters[i] = new Letter(this, rndChar, c, r);
 		}
+		
+		size(w, h);
 	}
 	
+	public World getWorld() { return world; }
+	
 	public void draw() {
+		background(0, 0, 0);
+		world.step(1.0f/60, 10);
+		
 		for(int i = 0; i < letters.length; i++) {
 			letters[i].draw();
 		}
+		world.setGravity( new Vec2(random(-10.5f, 10.5f), random(-10.5f, 10.5f)) );
 	}
 	
 	public static void main(String args[]) {
